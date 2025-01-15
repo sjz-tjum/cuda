62bc3d346c0173dbbf64ac037fce60fe2c2392c3
sjz-tjum <3884830295@qq.com>
2025-01-15 12:35:23 +0800

    Add files via upload

--- cuda/bayerprocessing.cu
+++ cuda/bayerprocessing.cu
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <cuda_runtime.h>  // 包含CUDA运行时头文件
+#include <cuda_runtime.h> 
 #include <opencv2/opencv.hpp>
 #include "common.cuh"
+#include  "readBayer.cuh"
+#include  "Mirror-Padding.cu"
 
 
 int main(void)
@@ -13,22 +15,40 @@ int main(void)
   readBayer_img Bayer{"example",width,height};
   std::vector<unsigned char> Bayer_data_ = Bayer.readBayerImage("example",width,height);
   std::vector<unsigned char> BGR_data_;
+  std::vector<unsigned char>& padded_data;
+  int pad_width = 10;
+  int padded_width = width + pad_width;
+  int padded_height = height + pad_width;
+  mirrorPadBayerImage(Bayer_data_ ,padded_data,width,height,pad_width);
   //分配主机内存，并初始化
-  int Bayer_ElemenCount = width*height;
-  int BGR_ElementCount = 3*width*height;
+  int Bayer_ElemenCount = ( width + pad_width ) * ( height + pad_width ) ;
+  int BGR_ElementCount = 3 * width * height ;
 
   size_t Bayer_Bytecount = Bayer_ElemenCount*sizeof(unsigned char);
   size_t Bgr_Bytecount = BGR_ElementCount * sizeof(unsigned char);
 
-  unsigned char *device_bayer_data; unsigned char*device_bgr_data;
+  unsigned char *device_padded_bayer_data; unsigned char*device_bgr_data;
   device_bayer_data = (float *)cudaMalloc(Bayer_Bytecount);
   device_bgr_data = (float *)cudaMalloc(Bgr_Bytecount);
   //复制主机数据到GPU
-  cudaMemcpy(device_bayer_data,Bayer_data_.data(), Bayer_Bytecount, cudaMemcpyHostToDevice);
+  cudaMemcpy(device_padded_bayer_data, padded_bayer_data.data(), padded_width * padded_height * sizeof(unsigned char), cudaMemcpyHostToDevice);
   //设置网格数和线程数
   dim3 threadsPerBlock(16, 16);
   dim3 blocksPerGrid((width + threadsPerBlock.x - 1) / threadsPerBlock.x, (height + threadsPerBlock.y - 1) / threadsPerBlock.y);
   
+  Demosaicing_process<<<blocksPerGrid, threadsPerBlock>>>(device_padded_bayer_data, device_bgr_data, padded_width, padded_height, width, height, pad_width);
+  cudaDeviceSynchronize();
 
+  std::vector<unsigned char> bgr_data(width * height * 3);
+  cudaMemcpy(bgr_data.data(), device_bgr_data, width * height * 3 * sizeof(unsigned char), cudaMemcpyDeviceToHost);
 
+  cudaFree(device_padded_bayer_data);
+  cudaFree(device_bgr_data);
+
+  
+  cv::Mat bgr_image(height, width, CV_8UC3, bgr_data.data());
+  cv::imwrite("output.jpg", bgr_image);
+
+  return 0;  
 }
+
--- cuda/src/processingD.cu
+++ cuda/src/processingD.cu
@@ -1,27 +1,34 @@
-#include"Demosaicing.cuh"
+#include<iostream>
+#include<stdlib.h>
+#include <cuda_runtime.h>
+#include <opencv2/opencv.hpp>
 
-__global__  void processDemosacing(unsigned char* device_bayer_data, unsigned char* device_bgr_data, int width, int height);
-{
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y;
-    if (x >= width || y >= height) return;
-    int bayer_idx = y * width + x;
-    int bgr_idx = bayer_idx * 3;
+__global__ void Demosaicing_process(unsigned char* padded_bayer_data, unsigned char* bgr_data, int padded_width, int padded_height, int width, int height,int pad_width) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x + pad_width;  
+    int y = blockIdx.y * blockDim.y + threadIdx.y + pad_width;  
 
-    if (y % 2 == 0) {
-        if (x % 2 == 0)
-        {
-            // B
-            device_bgr_data[bgr_idx + 0] = device_bayer_data[bayer_idx];
-            device_bgr_data[bgr_idx + 1] = (x + 1 < width) ? device_bayer_data[bayer_idx + 1] : device_bayer_data[bayer_idx - 1];
-            device_bgr_data[bgr_idx + 2] = (y + 1 < height) ? device_bayer_data[bayer_idx + width] : device_bayer_data[bayer_idx - width];
-        } 
-        else 
-        {
-            // G
-            device_bgr_data[bgr_idx + 0] = (x - 1 >= 0) ? device_bayer_data[bayer_idx - 1] : device_bayer_data[bayer_idx + 1];
-            device_bgr_data[bgr_idx + 1] = device_bayer_data[bayer_idx];
-            device_bgr_data[bgr_idx + 2] = (y + 1 < height) ? device_bayer_data[bayer_idx + width] : device_bayer_data[bayer_idx - width];
-        }
-    } 
+    if (x >= width + pad_width || y >= height + pad_width) return;
+    int bayer_idx = y * padded_width + x;
+    int bgr_idx = (y - pad_width) * width * 3 + (x - pad_width) * 3;
+    unsigned char r, g, b;
+    if (x % 2 == 0 && y % 2 == 0) { 
+        r = padded_bayer_data[bayer_idx];
+        g = (padded_bayer_data[bayer_idx - 1] + padded_bayer_data[bayer_idx + 1] + padded_bayer_data[bayer_idx - padded_width] + padded_bayer_data[bayer_idx + padded_width]) / 4;
+        b = (padded_bayer_data[bayer_idx - padded_width - 1] + padded_bayer_data[bayer_idx - padded_width + 1] + padded_bayer_data[bayer_idx + padded_width - 1] + padded_bayer_data[bayer_idx + padded_width + 1]) / 4;
+    } else if (x % 2 == 1 && y % 2 == 0) { 
+        r = (padded_bayer_data[bayer_idx - 1] + padded_bayer_data[bayer_idx + 1]) / 2;
+        g = padded_bayer_data[bayer_idx];
+        b = (padded_bayer_data[bayer_idx - padded_width] + padded_bayer_data[bayer_idx + padded_width] + padded_bayer_data[bayer_idx - padded_width - 1] + padded_bayer_data[bayer_idx - padded_width + 1] + padded_bayer_data[bayer_idx + padded_width - 1] + padded_bayer_data[bayer_idx + padded_width + 1]) / 6;
+    } else if (x % 2 == 0 && y % 2 == 1) { 
+        r = (padded_bayer_data[bayer_idx - padded_width] + padded_bayer_data[bayer_idx + padded_width]) / 2;
+        g = padded_bayer_data[bayer_idx];
+        b = (padded_bayer_data[bayer_idx - 1] + padded_bayer_data[bayer_idx + 1] + padded_bayer_data[bayer_idx - padded_width - 1] + padded_bayer_data[bayer_idx - padded_width + 1] + padded_bayer_data[bayer_idx + padded_width - 1] + padded_bayer_data[bayer_idx + padded_width + 1]) / 6;
+    } else { // B
+        r = (padded_bayer_data[bayer_idx - padded_width - 1] + padded_bayer_data[bayer_idx - padded_width + 1] + padded_bayer_data[bayer_idx + padded_width - 1] + padded_bayer_data[bayer_idx + padded_width + 1]) / 4;
+        g = (padded_bayer_data[bayer_idx - 1] + padded_bayer_data[bayer_idx + 1] + padded_bayer_data[bayer_idx - padded_width] + padded_bayer_data[bayer_idx + padded_width]) / 4;
+        b = padded_bayer_data[bayer_idx];
+    }
+    bgr_data[bgr_idx] = b;
+    bgr_data[bgr_idx + 1] = g;
+    bgr_data[bgr_idx + 2] = r;
 }
